07988203082218d0b8a8432f31e168251624fc69
will_saulon <willfrombuffalo@gmail.com>
2021-03-29 20:29:23 -0500

    pushed necessary files

---
+++ William_Saulon.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>About Me</title>
+    <link rel="stylesheet" type="text/css" href="cssFolder/myStyles.css">
+</head>
+
+<body>
+    <nav id="nav">
+        <div class="dropdown-about">
+            <button class="about-btn">AboutMe</button>
+            <div class="about-content">
+                <a class="links-hidden" href="https://github.com/williamrockowl/webdevcourse">GitHub</a>
+            </div>
+        </div>
+        <div class="dropdown-work">
+            <button class="work-btn">Schools I've Attended</button>
+            <div class="work-content">
+                <a class="links-hidden" href="https://www.ecc.edu/">ECC</a>
+                <a class="links-hidden" href="https://www.nyit.edu/">NYiT</a>
+                <a class="links-hidden" href="http://www.buffalo.edu/">UB</a>
+            </div>
+        </div>
+        <div class="dropdown-school">
+            <button class="school-btn">Places I've Worked</button>
+            <div class="school-content">
+                <a class="links-hidden" href="https://www.gnc.com/">GNC</a>
+                <a class="links-hidden" href="https://www.justforkidsonline.org/">Just For Kids</a>
+                <a class="links-hidden" href="https://www.brookstone.com/">Brookstone</a>
+            </div>
+        </div>
+        <a class="links" href="http://www.instagram.com/willfrombuffalo">Social Media</a>
+    </nav>
+    <div id="Introduction">
+        <hr />
+        <div id=Image>
+            <img src="Image\IMG_0908.jpg" alt="Hiking Trip" id="photo">
+        </div>
+        <h1>INTRODUCTION:</h1>
+        <p> Welcome to my About Me Page. My name's William Saulon
+            and I'm currently a full time student at <mark class="red">ECC</mark>.
+            The reason I decided on taking this course, was
+            to further my knowledge on full stack development. I've always enjoyed code and its infinite
+            capabilities. There is truely so much you can do with code and you can never get tired of it.
+            The tech world has so much opportunities career-wise as well as the impact it can have on the world.
+            Personally, I'd want to create a start up company from the very bottom. Before that, I would want to have
+            experience in
+            Web Design, CyberSecurity, Game Development, and possibly work for a big name company like <mark
+                class="blue">FaceBook</mark>
+            or <mark class="green">Google</mark>. Eventually, I would also want to go into the medical aspect of code
+            that would help
+            hospitals and doctors create new technology that can potentially save lives that we aren't able to save now.
+        </p>
+        <hr />
+        <h1>HISTORY:</h1>
+        <p> I was born on August 1999 in Queens, NY. I grew up with my parents and my little sister who was born two
+            years
+            after me. For elementary school, I went to PS20 while also attending a afternoon camp due to my parents
+            working all the way
+            in NYC. Growing up I would always be playing basketball, during lunch breaks in elementary I'd go straight
+            for the basketball
+            and shoot around with some friends. As I reached middle school, my living situations got worse and worse.
+            The community turned
+            south which caused a lot of panic in my family. After two years of being at IS237, my family decided it was
+            time to move to a
+            better location where I'd finish my last year of middle school. We moved to Long Island, NY which was a
+            40-60 min drive from where I originially grew
+            up. Everyone was super nice to me but it wasn't hard to tell that the friend groups around me weren't made
+            in a year. I eventually
+            was able to break out of my shell and started having fun. Entering High School, I made so many friends as
+            well as built a lot
+            of relationships with my teachers and advisors. I ended up joining the Baseball team as well as an AAU
+            Basketball team, while being part of Mathletes
+            and Yearbook Club. After HS I attended NYiT, then transferred to UB but decided to save money at ECC.
+        </p>
+        <hr />
+    </div>
+    <div id="ListOfJobs">
+        <h1>PLACES I HAVE WORKED:</h1>
+        <ul>
+            <li>General Nutrition Company : Sales Associate - (3/5 would recommend)</li>
+            <li>Just For Kids : Athletic Director - (3.5/5 would recommend)</li>
+            <li>Brookstone : Sales Associate - (4/5 would recommend)</li>
+            <li>Friends Pizzeria : Delivery Driver - (5/5 would definitely recommend)</li>
+            <li>Olive Garden : Busser - (2.5/5 would recommend)</li>
+            <li>Nassau County : Informational Technologist - (4/5 would recommend)</li>
+        </ul>
+    </div>
+    <hr />
+    <div id="PlacesLivedTable">
+        <h3>A TABLE OF WHERE I HAVE LIVED</h3>
+        <table class="center">
+            <thead>
+                <tr>
+                    <th>Location</th>
+                    <th>Timespan</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Queens, NY</td>
+                    <td>1999-2013</td>
+                </tr>
+                <tr>
+                    <td>Long Island, NY</td>
+                    <td>2013-Present</td>
+                </tr>
+                <tr>
+                    <td>Buffalo(School), NY</td>
+                    <td>2017-Present</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <hr />
+    <h1>LINKS:</h1>
+    <div class="linkstosites">
+        <p>
+            <a href="https://sites.google.com/site/buffalolittlelamb8/" target="_blank">~ CLICK HERE TO GO TO MY
+                FAVORITE RESTAURANT IN BUFFALO, NY ~</a>
+        </p>
+        <p>
+            <a href="secondPage.php" target="_blank">~ CLICK HERE TO GO TO MADLIBS! ~</a>
+        </p>
+        <p>
+            <a href="boot.php" target="_blank">~ BOOTSTRAP ~</a>
+        </p>
+    </div>
+    <hr />
+</body>
+
+</html>
